ba1612e7cab4ec4b82cde4070c5e366fc567deb2
dox404 <muktarul.hoque@hotmail.com>
2023-04-04 17:10:17 +0530

    inittal modification

--- src/components/Schema/index.js
+++ src/components/Schema/index.js
@@ -6,7 +6,7 @@ export const LoginSchema=yup.object().shape({
     password:yup.string().min(5).max(10).required("plese enter the password")
 })
 export const SignUpSchema=yup.object().shape({
-    fullname:yup.string().min(5,"enter valid name").max(30,"plese enter a valid name").required('Please Enter Your Name'),
+    fullname:yup.string().min(5,"Plese enter valid name").max(30,"Plese enter a valid name").required('Please Enter Your Name'),
     email:yup.string().email().required('Plese Enter your Email'),
     password:yup.string().min(5).max(10).required('Please Enter Your Password'),
     confirm_password:yup.string().required('plese confirm ur password').oneOf([yup.ref('password'),null],'Password Must Match')
