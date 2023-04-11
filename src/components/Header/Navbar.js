fb2db87c2898cd55fcb9749e17e5e41327a8d915
dox404 <muktarul.hoque@hotmail.com>
2023-04-11 23:13:40 +0530

    removed some unnecessary comment

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -43,31 +43,6 @@ const navigate=useNavigate()
 
 
 
-      {/* normal bootstrap with cdn links(cdn pasted in index.html file) */}
-      {/* <div className="p-0 m-0 border-0 bd-example">
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/"><img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="shoping Cart icon" className="navbar-logo" /></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/join">Join</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/about">About</a>
-                </li>
-
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div> */}
 
 
 
@@ -105,27 +80,5 @@ export default BasicExample;
 
 
 
-{/* <Navbar bg="black text-white" expand="lg">
-<Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="/login">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">
-          Another action
-        </NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">
-          Separated link
-        </NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-  </Navbar.Collapse>
-</Container>
-</Navbar> */}
+
 
